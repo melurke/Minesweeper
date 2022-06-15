08fff66d8fe6215cbf36e3f0f5178a2f5a60b763
Max Dörich <101022215+melurke@users.noreply.github.com>
2022-06-15 23:45:48 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -15,103 +15,106 @@ def scan_field(x_pos, y_pos, known, empties, ones, twos, threes, fours, fives, s
     field = [""] * 16
     empty_field = False
     for f in new_fields:
-        if color(f[0], f[1]) == "/":
+        if color(f[0], f[1]) == "0":
             empty_field = True
     if empty_field:
+        print("Scan full field:")
         for y in y_pos:
+            print(str(((y_pos.index(y))/len(y_pos))*100) + "%")
             for x in x_pos:
-                if not (x_pos.index(x)+1, y_pos.index(y)+1) in known:
+                if not (x, y) in known:
                     col = color(x, y)
-                    if col == "/":
-                        empties.append((x_pos.index(x)+1, y_pos.index(y)+1))
+                    if col == "0":
+                        empties.append((x, y))
                     elif col == "1":
-                        ones.append((x_pos.index(x)+1, y_pos.index(y)+1))
+                        ones.append((x, y))
                     elif col == "2":
-                        twos.append((x_pos.index(x)+1, y_pos.index(y)+1))
+                        twos.append((x, y))
                     elif col == "3":
-                        threes.append((x_pos.index(x)+1, y_pos.index(y)+1))
+                        threes.append((x, y))
                     elif col == "4":
-                        fours.append((x_pos.index(x)+1, y_pos.index(y)+1))
+                        fours.append((x, y))
                     elif col == "5":
-                        fives.append((x_pos.index(x)+1, y_pos.index(y)+1))
+                        fives.append((x, y))
                     elif col == "6":
-                        sixes.append((x_pos.index(x)+1, y_pos.index(y)+1))
+                        sixes.append((x, y))
                     elif col == "7":
-                        sevens.append((x_pos.index(x)+1, y_pos.index(y)+1))
+                        sevens.append((x, y))
                     elif col == "8":
-                        eights.append((x_pos.index(x)+1, y_pos.index(y)+1))
+                        eights.append((x, y))
                 else:
-                    if (x_pos.index(x)+1, y_pos.index(y)+1) in flags:
+                    if (x, y) in flags:
                         col = "!"
-                    elif (x_pos.index(x)+1, y_pos.index(y)+1) in ones:
+                    elif (x, y) in ones:
                         col = "1"
-                    elif (x_pos.index(x)+1, y_pos.index(y)+1) in twos:
+                    elif (x, y) in twos:
                         col = "2"
-                    elif (x_pos.index(x)+1, y_pos.index(y)+1) in threes:
+                    elif (x, y) in threes:
                         col = "3"
-                    elif (x_pos.index(x)+1, y_pos.index(y)+1) in fours:
+                    elif (x, y) in fours:
                         col = "4"
-                    elif (x_pos.index(x)+1, y_pos.index(y)+1) in fives:
+                    elif (x, y) in fives:
                         col = "5"
-                    elif (x_pos.index(x)+1, y_pos.index(y)+1) in sixes:
+                    elif (x, y) in sixes:
                         col = "6"
-                    elif (x_pos.index(x)+1, y_pos.index(y)+1) in sevens:
+                    elif (x, y) in sevens:
                         col = "7"
-                    elif (x_pos.index(x)+1, y_pos.index(y)+1) in eights:
+                    elif (x, y) in eights:
                         col = "8"
-                    elif (x_pos.index(x)+1, y_pos.index(y)+1) in empties:
-                        col = "/"
-                    else:
+                    elif (x, y) in empties:
                         col = "0"
+                    else:
+                        col = "/"
                 field[y_pos.index(y)] += col
     else:
+        print("Scan neighbors:")
         for y in y_pos:
+            print(str(((y_pos.index(y))/len(y_pos))*100) + "%")
             for x in x_pos:
-                if (x_pos.index(x)+1, y_pos.index(y)+1) in known:
-                    if (x_pos.index(x)+1, y_pos.index(y)+1) in flags:
+                if (x, y) in known:
+                    if (x, y) in flags:
                         col = "!"
-                    elif (x_pos.index(x)+1, y_pos.index(y)+1) in ones:
+                    elif (x, y) in ones:
                         col = "1"
-                    elif (x_pos.index(x)+1, y_pos.index(y)+1) in twos:
+                    elif (x, y) in twos:
                         col = "2"
-                    elif (x_pos.index(x)+1, y_pos.index(y)+1) in threes:
+                    elif (x, y) in threes:
                         col = "3"
-                    elif (x_pos.index(x)+1, y_pos.index(y)+1) in fours:
+                    elif (x, y) in fours:
                         col = "4"
-                    elif (x_pos.index(x)+1, y_pos.index(y)+1) in fives:
+                    elif (x, y) in fives:
                         col = "5"
-                    elif (x_pos.index(x)+1, y_pos.index(y)+1) in sixes:
+                    elif (x, y) in sixes:
                         col = "6"
-                    elif (x_pos.index(x)+1, y_pos.index(y)+1) in sevens:
+                    elif (x, y) in sevens:
                         col = "7"
-                    elif (x_pos.index(x)+1, y_pos.index(y)+1) in eights:
+                    elif (x, y) in eights:
                         col = "8"
-                    elif (x_pos.index(x)+1, y_pos.index(y)+1) in empties:
-                        col = "/"
+                    elif (x, y) in empties:
+                        col = "0"
                 else:
-                    col = "0"
+                    col = "/"
                 field[y_pos.index(y)] += col
     return field
 
 def color(x, y):
     col = pyautogui.pixel(x, y)
     col_2 = pyautogui.pixel(x, y+1)
-    if col == empty:
-        if pyautogui.pixel(x-12, y-12) == unknown:
-            return "0"
-        else:
-            return "/"
-    elif col == one:
+    if pyautogui.pixel(x-12, y-12) == unknown:
+        return "/"
+    elif col[2] == 255:
         return "1"
+    elif col[0] == 189:
+        return "0"
     elif col_2 == two:
         return "2"
-    elif col_2 == three:
+    elif col_2[0] == 255:
         return "3"
     elif pyautogui.pixel(x+2, y) == four:
         return "4"
     elif col_2 == five:
         return "5"
-    elif pyautogui.pixel(x-1, y) == six:
+    elif col_2 == six:
         return "6"
     elif pyautogui.pixel(x, y+5) == seven:
         return "7"
@@ -130,35 +133,101 @@ def neighbors(x, x_pos, y, y_pos):
                     n.append((x_, y_))
     return n
     
-def flags(neighbors, num_of_neighbors, x_pos, y_pos, known, flags):
+def flags(neighbors, num_of_neighbors, known, flags):
     empty_n = []
     num_of_empties = 0
 
     for N in neighbors:
-        coords = (x_pos.index(N[0])+1, y_pos.index(N[1])+1)
-        if not coords in known:
+        if not N in known:
             num_of_empties += 1
-            empty_n.append((coords[0]-1, coords[1]-1))
-        elif coords in flags:
+            empty_n.append(N)
+        elif N in flags:
             num_of_empties += 1
     if num_of_empties == num_of_neighbors:
+        for C in empty_n:
+            pyautogui.rightClick(C[0], C[1])
         return [empty_n, True]
     else:
-        return [empty_n, False]
+        return [[], False]
 
-def no_mines(neighbors, x_pos, y_pos, known, flags, num_of_neighbors):
+def no_mines(neighbors, known, flags, num_of_neighbors):
     num_of_flags = 0
     for n in neighbors:
-        coords = (x_pos.index(n[0])+1, y_pos.index(n[1])+1)
-        if coords in flags:
+        if n in flags:
             num_of_flags += 1
     if num_of_flags == num_of_neighbors:
         num_of_empty_neighbors = 0
         empty_neighbors = []
         for n in neighbors:
-            coords = (x_pos.index(n[0])+1, y_pos.index(n[1])+1)
-            if not coords in known:
+            if not n in known:
                 num_of_empty_neighbors += 1
                 empty_neighbors.append(n)
+                pyautogui.click(n[0], n[1])
         return [empty_neighbors, True]
     return [[], False]
+
+def guess(x_pos, y_pos, known, empties, flags, ones, twos, threes, fours, fives, sixes, sevens, eights):
+    probabilities = [[1, 0]] * 480
+
+    for C in known:
+        num_of_flags = 0
+        if not C in empties and not C in flags:
+            if C in ones:
+                num_of_flags = 1
+            elif C in twos:
+                num_of_flags = 2
+            elif C in threes:
+                num_of_flags = 3
+            elif C in fours:
+                num_of_flags = 4
+            elif C in fives:
+                num_of_flags = 5
+            elif C in sixes:
+                num_of_flags = 6
+            elif C in sevens:
+                num_of_flags = 7
+            elif C in eights:
+                num_of_flags = 8
+            n = neighbors(C[0], x_pos, C[1], y_pos)
+            empty_n = []
+            num_of_free_neighbors = 0
+            num_of_free_flags = 0
+            num_of_used_flags = 0
+            for N in n:
+                if N in flags:
+                    num_of_used_flags += 1
+                elif N in empty:
+                    num_of_free_neighbors += 1
+                    empty_n.append(N)
+            print(C)
+            print(empty_n)
+            num_of_free_flags = num_of_flags - num_of_used_flags
+            try:
+                p = num_of_free_flags / num_of_free_neighbors
+            except ZeroDivisionError:
+                p = 20
+            # FIXME:
+            for N in empty_n:
+                x_coord = N[0]
+                y_coord = N[1]
+                probabilities[y_coord*16+x_coord][0] += p
+                probabilities[y_coord*16+x_coord][1] += 1
+                print(probabilities[y_coord*16+x_coord])
+    new_probabilities = []
+    print(probabilities)
+    for b in probabilities.copy():
+        try:
+            c = b[0] / b[1]
+        except ZeroDivisionError:
+            c = 1
+        new_probabilities.append(c)
+    print(new_probabilities)
+    probabilities = new_probabilities.copy()
+    min_index = probabilities.index(min(probabilities))
+    x_index = min_index % 16
+    y_index = int((min_index-x_index)/16)
+
+    coords = (x_pos[x_index], y_pos[y_index])
+    print(probabilities)
+    print(x_index, y_index)
+    return coords
