b94fa416f1e1388b53ab270f3ca6299737be01f9
Max Dörich <101022215+melurke@users.noreply.github.com>
2022-06-15 23:46:06 +0200

    Create cool_bot.py

---
+++ cool_bot.py
@@ -0,0 +1,14 @@
+import win32api
+import time
+import win32con
+import keyboard
+
+def click(x, y):
+    win32api.SetCursorPos((x, y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+    time.sleep(0.02)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+
+while not keyboard.is_pressed('q'):
+    click(188, 178)
+    click(188, 348)
