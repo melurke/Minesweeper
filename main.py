d0f0a5b8ff21f430e3ace00a10ccc8e073302b2e
Max Dörich <101022215+melurke@users.noreply.github.com>
2022-06-15 23:45:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pyautogui
-import time
 import functions as f
 
 x_pos = [69, 97, 125, 153, 181, 209, 237, 265, 293, 321, 349, 377, 405, 433, 461, 489, 517, 545, 573, 601, 629, 657, 685, 713, 741, 769, 797, 825, 853, 881]
@@ -19,13 +18,14 @@ known = []
 new_fields = []
 clicked = True
 first_round = True
+end_game = False
 
 while True:
     pyautogui.moveTo(1000, 500)
     field = [""] * 16
     for pi in new_fields:
         col = f.color(pi[0], pi[1])
-        if col == "/":
+        if col == "0":
             empties.append(pi)
         elif col == "1":
             ones.append(pi)
@@ -49,91 +49,93 @@ while True:
         if clicked:
             field = f.scan_field(x_pos, y_pos, known, empties, ones, twos, threes, fours, fives, sixes, sevens, eights, flags, new_fields)
         else:
-            for y in range(0, 16):
-                for x in range(0, 30):
-                    if (y+1, x+1) in flags:
+            print("Get known fields:")
+            for y in y_pos:
+                print(str(((y_pos.index(y))/len(y_pos))*100) + "%")
+                for x in x_pos:
+                    if (x, y) in flags:
                         field[y] += "!"
-                    elif (y+1, x+1) in ones:
+                    elif (x, y) in ones:
                         field[y] += "1"
-                    elif (y+1, x+1) in twos:
+                    elif (x, y) in twos:
                         field[y] += "2"
-                    elif (y+1, x+1) in threes:
+                    elif (x, y) in threes:
                         field[y] += "3"
-                    elif (y+1, x+1) in fours:
+                    elif (x, y) in fours:
                         field[y] += "4"
-                    elif (y+1, x+1) in fives:
+                    elif (x, y) in fives:
                         field[y] += "5"
-                    elif (y+1, x+1) in sixes:
+                    elif (x, y) in sixes:
                         field[y] += "6"
-                    elif (y+1, x+1) in sevens:
+                    elif (x, y) in sevens:
                         field[y] += "7"
-                    elif (y+1, x+1) in eights:
+                    elif (x, y) in eights:
                         field[y] += "8"
-                    elif (y+1, x+1) in empties:
-                        field[y] += "/"
-                    else:
+                    elif (x, y) in empties:
                         field[y] += "0"
+                    else:
+                        field[y] += "/"
     else:
+        print("Scan full field:")
         for y in y_pos:
+            print(str(((y_pos.index(y))/len(y_pos))*100) + "%")
             for x in x_pos:
-                if not (x_pos.index(x)+1, y_pos.index(y)+1) in known:
+                if not (x, y) in known:
                     col = f.color(x, y)
-                    if col == "/":
-                        empties.append((x_pos.index(x)+1, y_pos.index(y)+1))
+                    if col == "0":
+                        empties.append((x, y))
                     elif col == "1":
-                        ones.append((x_pos.index(x)+1, y_pos.index(y)+1))
+                        ones.append((x, y))
                     elif col == "2":
-                        twos.append((x_pos.index(x)+1, y_pos.index(y)+1))
+                        twos.append((x, y))
                     elif col == "3":
-                        threes.append((x_pos.index(x)+1, y_pos.index(y)+1))
+                        threes.append((x, y))
                     elif col == "4":
-                        fours.append((x_pos.index(x)+1, y_pos.index(y)+1))
+                        fours.append((x, y))
                     elif col == "5":
-                        fives.append((x_pos.index(x)+1, y_pos.index(y)+1))
+                        fives.append((x, y))
                     elif col == "6":
-                        sixes.append((x_pos.index(x)+1, y_pos.index(y)+1))
+                        sixes.append((x, y))
                     elif col == "7":
-                        sevens.append((x_pos.index(x)+1, y_pos.index(y)+1))
+                        sevens.append((x, y))
                     elif col == "8":
-                        eights.append((x_pos.index(x)+1, y_pos.index(y)+1))
+                        eights.append((x, y))
                 else:
-                    if (x_pos.index(x)+1, y_pos.index(y)+1) in flags:
+                    if (x, y) in flags:
                         col = "!"
-                    elif (x_pos.index(x)+1, y_pos.index(y)+1) in ones:
+                    elif (x, y) in ones:
                         col = "1"
-                    elif (x_pos.index(x)+1, y_pos.index(y)+1) in twos:
+                    elif (x, y) in twos:
                         col = "2"
-                    elif (x_pos.index(x)+1, y_pos.index(y)+1) in threes:
+                    elif (x, y) in threes:
                         col = "3"
-                    elif (x_pos.index(x)+1, y_pos.index(y)+1) in fours:
+                    elif (x, y) in fours:
                         col = "4"
-                    elif (x_pos.index(x)+1, y_pos.index(y)+1) in fives:
+                    elif (x, y) in fives:
                         col = "5"
-                    elif (x_pos.index(x)+1, y_pos.index(y)+1) in sixes:
+                    elif (x, y) in sixes:
                         col = "6"
-                    elif (x_pos.index(x)+1, y_pos.index(y)+1) in sevens:
+                    elif (x, y) in sevens:
                         col = "7"
-                    elif (x_pos.index(x)+1, y_pos.index(y)+1) in eights:
+                    elif (x, y) in eights:
                         col = "8"
-                    elif (x_pos.index(x)+1, y_pos.index(y)+1) in empties:
-                        col = "/"
+                    elif (x, y) in empties:
+                        col = "0"
                 field[y_pos.index(y)] += col
     for row in field:
         print(row)
+    known_fields = known.copy()
     print("")
     print("")
-    clicked = False
 
-    known = flags + ones + twos + threes + fours + sixes + sevens + eights + empties
+    clicked = False
+    known = flags + ones + twos + threes + fours + fives + sixes + sevens + eights + empties
     known = list(dict.fromkeys(known))
     known.sort()
 
     for C in known.copy():
-        pass_field = False
         num_of_neighbors = 0
-        if C in flags or C in empties:
-            pass_field = True
-        if not pass_field:
+        if not C in flags and not C in empties:
             if C in ones:
                 num_of_neighbors = 1
             elif C in twos:
@@ -150,29 +152,19 @@ while True:
                 num_of_neighbors = 7
             elif C in eights:
                 num_of_neighbors = 8
-            
-            x_coord = x_pos[C[0]-1]
-            y_coord = y_pos[C[1]-1]
 
-            n = f.flags(f.neighbors(x_coord, x_pos, y_coord, y_pos), num_of_neighbors, x_pos, y_pos, known, flags)
+            n = f.flags(f.neighbors(C[0], x_pos, C[1], y_pos), num_of_neighbors, known, flags)
             
             if n[1]:
-                for E in n[0]:
-                    x_coord = x_pos[E[0]]
-                    y_coord = y_pos[E[1]]
-                    pyautogui.rightClick(x_coord, y_coord)
-                    flags.append((E[0]+1, E[1]+1))
-                    known.append((E[0]+1, E[1]+1))
+                flags += n[0]
+                known += n[0]
 
     new_fields = []
 
     for C in known.copy():
-        pass_field = False
         num_of_neighbors = 0
 
-        if C in flags or C in empties:
-            pass_field = True
-        if not pass_field:
+        if not C in flags and not C in empties:
             if C in ones:
                 num_of_neighbors = 1
             elif C in twos:
@@ -190,17 +182,19 @@ while True:
             elif C in eights:
                 num_of_neighbors = 8
 
-            x_coord = x_pos[C[0]-1]
-            y_coord = y_pos[C[1]-1]
+            n = f.no_mines(f.neighbors(C[0], x_pos, C[1], y_pos), known, flags, num_of_neighbors)
 
-            n = f.no_mines(f.neighbors(x_coord, x_pos, y_coord, y_pos), x_pos, y_pos, known, flags, num_of_neighbors)
-            
+            new_fields += n[0]
             if n[1]:
-                for E in n[0]:
-                    pyautogui.leftClick(E[0], E[1])
-                    new_fields.append((x_pos.index(E[0])+1, y_pos.index(E[1])+1))
-                    clicked = True
+                clicked = True
+
+    if known == known_fields:
+        print("Taking guess:")
+        guess = f.guess(x_pos, y_pos, known, empties, flags, ones, twos, threes, fours, fives, sixes, sevens, eights)
+        pyautogui.click(guess[0], guess[1])
+        break
     if pyautogui.locateOnScreen("smiley.png") != None:
         break
-    time.sleep(1)
     first_round = False
+print("")
+print("The game ended!")
