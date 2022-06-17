c13c8e09809494aa4587ac4fa8aabea8fcad984d
Max Dörich <101022215+melurke@users.noreply.github.com>
2022-06-17 15:10:27 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
-import pyautogui
+import win32api
+import time
+import win32con
 
 unknown = (255, 255, 255)
 empty = (189, 189, 189)
@@ -11,96 +13,75 @@ six = (0, 123, 123)
 seven = (0, 0, 0)
 eight = (123, 123, 123)
 
-def scan_field(x_pos, y_pos, known, empties, ones, twos, threes, fours, fives, sixes, sevens, eights, flags, new_fields):
-    field = [""] * 16
+def leftClick(x, y):
+    win32api.SetCursorPos((x, y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+    time.sleep(0.02)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+
+def rightClick(x, y):
+    win32api.SetCursorPos((x, y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, 0, 0)
+    time.sleep(0.02)
+    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, 0, 0)
+
+def scan_field(img, x_pos, y_pos, known, empties, ones, twos, threes, fours, fives, sixes, sevens, eights, flags, new_fields):
+    field = ""
     empty_field = False
+    empty_fields = []
     for f in new_fields:
-        if color(f[0], f[1]) == "0":
+        if color(img, f[0], f[1]) == "0":
             empty_field = True
+            empty_fields.append(f)
     if empty_field:
-        print("Scan full field:")
-        for y in y_pos:
-            print(str(((y_pos.index(y))/len(y_pos))*100) + "%")
-            for x in x_pos:
-                if not (x, y) in known:
-                    col = color(x, y)
+        print("Scan neighbors of empty fields")
+        field = "/" * 480
+        for C in known:
+            x_coord = x_pos.index(C[0])
+            y_coord = y_pos.index(C[1])
+            T = type(C[0], C[1], known, ones, twos, threes, fours, fives, sixes, sevens, eights, empties, flags)
+            field = field[:y_coord*30+x_coord] + T + field[y_coord*30+x_coord+1:]
+        for C in empty_fields:
+            n = neighbors(C[0], x_pos, C[1], y_pos)
+            for N in n:
+                if not N in known:
+                    col = color(img, N[0], N[1])
                     if col == "0":
-                        empties.append((x, y))
+                        empties.append(N)
+                        empty_fields.append(N)
                     elif col == "1":
-                        ones.append((x, y))
+                        ones.append(N)
                     elif col == "2":
-                        twos.append((x, y))
+                        twos.append(N)
                     elif col == "3":
-                        threes.append((x, y))
+                        threes.append(N)
                     elif col == "4":
-                        fours.append((x, y))
+                        fours.append(N)
                     elif col == "5":
-                        fives.append((x, y))
+                        fives.append(N)
                     elif col == "6":
-                        sixes.append((x, y))
+                        sixes.append(N)
                     elif col == "7":
-                        sevens.append((x, y))
+                        sevens.append(N)
                     elif col == "8":
-                        eights.append((x, y))
-                else:
-                    if (x, y) in flags:
-                        col = "!"
-                    elif (x, y) in ones:
-                        col = "1"
-                    elif (x, y) in twos:
-                        col = "2"
-                    elif (x, y) in threes:
-                        col = "3"
-                    elif (x, y) in fours:
-                        col = "4"
-                    elif (x, y) in fives:
-                        col = "5"
-                    elif (x, y) in sixes:
-                        col = "6"
-                    elif (x, y) in sevens:
-                        col = "7"
-                    elif (x, y) in eights:
-                        col = "8"
-                    elif (x, y) in empties:
-                        col = "0"
-                    else:
-                        col = "/"
-                field[y_pos.index(y)] += col
+                        eights.append(N)
+                    known.append(N)
+                    
+                    x_coord = x_pos.index(N[0])
+                    y_coord = y_pos.index(N[1])
+                    field = field[:y_coord*20+x_coord] + col + field[y_coord*30+x_coord+1:]
     else:
-        print("Scan neighbors:")
+        print("Scan neighbors")
         for y in y_pos:
-            print(str(((y_pos.index(y))/len(y_pos))*100) + "%")
             for x in x_pos:
-                if (x, y) in known:
-                    if (x, y) in flags:
-                        col = "!"
-                    elif (x, y) in ones:
-                        col = "1"
-                    elif (x, y) in twos:
-                        col = "2"
-                    elif (x, y) in threes:
-                        col = "3"
-                    elif (x, y) in fours:
-                        col = "4"
-                    elif (x, y) in fives:
-                        col = "5"
-                    elif (x, y) in sixes:
-                        col = "6"
-                    elif (x, y) in sevens:
-                        col = "7"
-                    elif (x, y) in eights:
-                        col = "8"
-                    elif (x, y) in empties:
-                        col = "0"
-                else:
-                    col = "/"
-                field[y_pos.index(y)] += col
+                col = type(x, y, known, ones, twos, threes, fours, fives, sixes, sevens, eights, empties, flags)
+                field += col
     return field
 
-def color(x, y):
-    col = pyautogui.pixel(x, y)
-    col_2 = pyautogui.pixel(x, y+1)
-    if pyautogui.pixel(x-12, y-12) == unknown:
+def color(img, x, y):
+    col = img.getpixel((x, y))
+    col_2 = img.getpixel((x, y+1))
+    if img.getpixel((x-12, y-12)) == unknown:
         return "/"
     elif col[2] == 255:
         return "1"
@@ -110,16 +91,14 @@ def color(x, y):
         return "2"
     elif col_2[0] == 255:
         return "3"
-    elif pyautogui.pixel(x+2, y) == four:
+    elif img.getpixel((x+2, y)) == four:
         return "4"
     elif col_2 == five:
         return "5"
     elif col_2 == six:
         return "6"
-    elif pyautogui.pixel(x, y+5) == seven:
+    elif img.getpixel((x, y+5)) == seven:
         return "7"
-    elif col == eight:
-        return "8"
     return "!"
 
 def neighbors(x, x_pos, y, y_pos):
@@ -145,7 +124,7 @@ def flags(neighbors, num_of_neighbors, known, flags):
             num_of_empties += 1
     if num_of_empties == num_of_neighbors:
         for C in empty_n:
-            pyautogui.rightClick(C[0], C[1])
+            rightClick(C[0], C[1])
         return [empty_n, True]
     else:
         return [[], False]
@@ -162,68 +141,75 @@ def no_mines(neighbors, known, flags, num_of_neighbors):
             if not n in known:
                 num_of_empty_neighbors += 1
                 empty_neighbors.append(n)
-                pyautogui.click(n[0], n[1])
+                leftClick(n[0], n[1])
         return [empty_neighbors, True]
     return [[], False]
+
+def type(x, y, known, ones, twos, threes, fours, fives, sixes, sevens, eights, empties, flags):
+    pos = (x, y)
+    if not pos in known:
+        T = "/"
+    elif pos in empties:
+        T = "0"
+    elif pos in flags:
+        T = "!"
+    elif pos in ones:
+        T = "1"
+    elif pos in twos:
+        T = "2"
+    elif pos in threes:
+        T = "3"
+    elif pos in fours:
+        T = "4"
+    elif pos in fives:
+        T = "5"
+    elif pos in sixes:
+        T = "6"
+    elif pos in sevens:
+        T = "7"
+    elif pos in eights:
+        T = "8"
+    return T
 
 def guess(x_pos, y_pos, known, empties, flags, ones, twos, threes, fours, fives, sixes, sevens, eights):
     probabilities = [[1, 0]] * 480
 
     for C in known:
-        num_of_flags = 0
+        probabilities[(y_pos.index(C[1])*30+x_pos.index(C[0]))] = [0, 0]
         if not C in empties and not C in flags:
-            if C in ones:
-                num_of_flags = 1
-            elif C in twos:
-                num_of_flags = 2
-            elif C in threes:
-                num_of_flags = 3
-            elif C in fours:
-                num_of_flags = 4
-            elif C in fives:
-                num_of_flags = 5
-            elif C in sixes:
-                num_of_flags = 6
-            elif C in sevens:
-                num_of_flags = 7
-            elif C in eights:
-                num_of_flags = 8
+            num_of_flags = int(type(C[0], C[1], known, ones, twos, threes, fours, fives, sixes, sevens, eights, empties, flags))
             n = neighbors(C[0], x_pos, C[1], y_pos)
             empty_n = []
             num_of_free_neighbors = 0
-            num_of_free_flags = 0
             num_of_used_flags = 0
             for N in n:
-                if N in flags:
-                    num_of_used_flags += 1
-                elif N in empties:
-                    num_of_free_neighbors += 1
+                if N in empties:
                     empty_n.append(N)
+                    num_of_free_neighbors += 1
+                elif N in flags:
+                    num_of_used_flags += 1
             num_of_free_flags = num_of_flags - num_of_used_flags
             try:
                 p = num_of_free_flags / num_of_free_neighbors
             except ZeroDivisionError:
-                p = 20
-            # FIXME:
+                p = 0
             for N in empty_n:
                 x_coord = x_pos.index(N[0])
                 y_coord = y_pos.index(N[1])
-                probabilities[y_coord*16+x_coord][0] += p
-                probabilities[y_coord*16+x_coord][1] += 1
+                if p != 0:
+                    probabilities[y_coord*30+x_coord][0] += p
+                    probabilities[y_coord*30+x_coord][1] += 1
     new_probabilities = []
     for b in probabilities.copy():
         try:
-            c = b[0] / b[1]
+            c = round(b[0] / b[1], 2)
         except ZeroDivisionError:
             c = 1
         new_probabilities.append(c)
-    print(new_probabilities)
-    probabilities = new_probabilities.copy()
-    min_index = probabilities.index(min(probabilities))
-    x_index = min_index % 16
+    min_index = new_probabilities.index(min(new_probabilities))
+    x_index = min_index % 30
     y_index = int((min_index-x_index)/16)
-
     coords = (x_pos[x_index], y_pos[y_index])
-    print(probabilities)
-    print(x_index, y_index)
+    print(new_probabilities)
+    print(f"{x_index}, {y_index}: {new_probabilities[min_index]}")
     return coords
