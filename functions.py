83f54f96d164d9a6cbd068da37808511112256bd
melurke <standartbrostv@gmail.com>
2022-06-18 18:42:46 +0200

    Update Code

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+from numpy import zeros
 import win32api
 import time
 import win32con
@@ -25,60 +26,69 @@ def rightClick(x, y):
     time.sleep(0.02)
     win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, 0, 0)
 
-def scan_field(img, x_pos, y_pos, known, empties, ones, twos, threes, fours, fives, sixes, sevens, eights, new_fields):
-    empty_field = False
-    empty_fields = []
-    for f in new_fields:
-        if color(img, f[0], f[1]) == "0":
-            empty_field = True
-            empty_fields.append(f)
-    if empty_field:
-        for C in empty_fields:
-            n = neighbors(C[0], x_pos, C[1], y_pos)
-            for N in n:
-                if not N in known:
-                    col = color(img, N[0], N[1])
-                    if col == "0":
-                        empties.append(N)
-                        empty_fields.append(N)
-                    elif col == "1":
-                        ones.append(N)
-                    elif col == "2":
-                        twos.append(N)
-                    elif col == "3":
-                        threes.append(N)
-                    elif col == "4":
-                        fours.append(N)
-                    elif col == "5":
-                        fives.append(N)
-                    elif col == "6":
-                        sixes.append(N)
-                    elif col == "7":
-                        sevens.append(N)
-                    elif col == "8":
-                        eights.append(N)
-                    known.append(N)
+def append_to_list(type, x, y, known, flags, empties, ones, twos, threes, fours, fives, sixes, sevens, eights):
+    if type == "1":
+        ones.append((x, y))
+        known.append((x, y))
+    elif type == "2":
+        twos.append((x, y))
+        known.append((x, y))
+    elif type == "3":
+        threes.append((x, y))
+        known.append((x, y))
+    elif type == "4":
+        fours.append((x, y))
+        known.append((x, y))
+    elif type == "5":
+        fives.append((x, y))
+        known.append((x, y))
+    elif type == "6":
+        sixes.append((x, y))
+        known.append((x, y))
+    elif type == "7":
+        sevens.append((x, y))
+        known.append((x, y))
+    elif type == "8":
+        eights.append((x, y))
+        known.append((x, y))
+    elif type == "!":
+        flags.append((x, y))
+        known.append((x, y))
+    elif type == "0":
+        empties.append((x, y))
+        known.append((x, y))
+
+def scan_field(img, x_pos, y_pos, known, empties, ones, twos, threes, fours, fives, sixes, sevens, eights, flags):
+    field = [""] * 16
+    for iy, y in enumerate(y_pos):
+        for x in x_pos:
+            col = color(img, x, y)
+            field[iy] += col
+            append_to_list(col, x, y, known, flags, empties, ones, twos, threes, fours, fives, sixes, sevens, eights)
+    return field
 
 def color(img, x, y):
-    col = img.getpixel((x, y))
-    col_2 = img.getpixel((x, y+1))
-    if img.getpixel((x-12, y-12)) == unknown:
+    col = img[x, y]
+    col_2 = img[x, y+1]
+    if img[x-12, y-12] == unknown:
+        if img[x-1, y+5][0] == 21:
+            return "!"
         return "/"
     elif col[2] == 255:
         return "1"
-    elif col[0] == 189:
-        return "0"
     elif col_2 == two:
         return "2"
+    elif col[0] == 189:
+        return "0"
     elif col_2[0] == 255:
         return "3"
-    elif img.getpixel((x+2, y)) == four:
+    elif img[x+2, y] == four:
         return "4"
     elif col_2 == five:
         return "5"
     elif col_2 == six:
         return "6"
-    elif img.getpixel((x, y+5)) == seven:
+    elif img[x, y+5] == seven:
         return "7"
     return "!"
 
@@ -106,9 +116,8 @@ def flags(neighbors, num_of_neighbors, known, flags):
     if num_of_empties == num_of_neighbors:
         for C in empty_n:
             rightClick(C[0], C[1])
-        return [empty_n, True]
-    else:
-        return [[], False]
+            known.append(C)
+            flags.append(C)
 
 def no_mines(neighbors, known, flags, num_of_neighbors):
     num_of_flags = 0
@@ -123,11 +132,13 @@ def no_mines(neighbors, known, flags, num_of_neighbors):
                 num_of_empty_neighbors += 1
                 empty_neighbors.append(n)
                 leftClick(n[0], n[1])
+                known.append(n)
         return [empty_neighbors, True]
     return [[], False]
 
 def type(x, y, known, ones, twos, threes, fours, fives, sixes, sevens, eights, empties, flags):
     pos = (x, y)
+    T = ""
     if not pos in known:
         T = "/"
     elif pos in empties:
@@ -153,47 +164,44 @@ def type(x, y, known, ones, twos, threes, fours, fives, sixes, sevens, eights, e
     return T
 
 def guess(x_pos, y_pos, known, empties, flags, ones, twos, threes, fours, fives, sixes, sevens, eights):
-    probabilities = [[1, 0]] * 480
-
-    for C in known:
-        probabilities[(y_pos.index(C[1])*30+x_pos.index(C[0]))] = [0, 0]
-        if not C in empties and not C in flags:
-            num_of_flags = int(type(C[0], C[1], known, ones, twos, threes, fours, fives, sixes, sevens, eights, empties, flags))
-            n = neighbors(C[0], x_pos, C[1], y_pos)
-            empty_n = []
-            num_of_free_neighbors = 0
-            num_of_used_flags = 0
-            for N in n:
-                if N in empties:
-                    empty_n.append(N)
-                    num_of_free_neighbors += 1
-                elif N in flags:
-                    num_of_used_flags += 1
-            num_of_free_flags = num_of_flags - num_of_used_flags
-            try:
-                p = num_of_free_flags / num_of_free_neighbors
-            except ZeroDivisionError:
+    probabilities = []
+    for y in y_pos:
+        for x in x_pos:
+            if not (x, y) in known:
+                n = neighbors(x, x_pos, y, y_pos)
                 p = 0
-            for N in empty_n:
-                x_coord = x_pos.index(N[0])
-                y_coord = y_pos.index(N[1])
-                if p != 0:
-                    probabilities[y_coord*30+x_coord][0] += p
-                    probabilities[y_coord*30+x_coord][1] += 1
-    new_probabilities = []
-    for b in probabilities.copy():
-        try:
-            c = b[0] / b[1]
-        except ZeroDivisionError:
-            c = 1
-        new_probabilities.append(c)
-    min_index = new_probabilities.index(min(new_probabilities))
+                num_of_known_neighbors = 0
+                for N in n:
+                    if N in known and not N in flags:
+                        num_of_known_neighbors += 1
+
+                        n_n = neighbors(N[0], x_pos, N[1], y_pos)
+                        num_of_used_flags = 0
+                        num_of_empty_neighbors = 0
+                        for C in n_n:
+                            if C in flags:
+                                num_of_used_flags += 1
+                            elif C in empties:
+                                num_of_empty_neighbors += 1
+                        num_of_flags = int(type(N[0], N[1], known, ones, twos, threes, fours, fives, sixes, sevens, eights, empties, flags))
+                        num_of_needed_flags = num_of_flags - num_of_used_flags
+                        try:
+                            p += num_of_needed_flags / num_of_empty_neighbors
+                        except ZeroDivisionError:
+                            pass
+
+                try:
+                    p /= num_of_known_neighbors
+                except ZeroDivisionError:
+                    p = 1
+                probabilities.append(p)
+            else:
+                probabilities.append(1)
+
+    min_index = probabilities.index(min(probabilities))
     x_index = min_index % 30
-    y_index = int((min_index-x_index)/16)
-    try:
-        coords = (x_pos[x_index], y_pos[y_index])
-        print(new_probabilities)
-        print(f"{x_index}, {y_index}: {new_probabilities[min_index]}")
-        return coords
-    except:
-        return
+    y_index = int((min_index - x_index) / 30)
+    coords = (x_pos[x_index], y_pos[y_index])
+    print(probabilities)
+    print(coords, probabilities[min_index])
+    return coords
--- main.py
+++ main.py
@@ -1,104 +1,54 @@
 import pyautogui
+from PIL import Image
 import functions as f
 import keyboard
 import time
 
 x_pos = [69, 97, 125, 153, 181, 209, 237, 265, 293, 321, 349, 377, 405, 433, 461, 489, 517, 545, 573, 601, 629, 657, 685, 713, 741, 769, 797, 825, 853, 881]
 y_pos = [233, 261, 289, 317, 345, 373, 401, 429, 457, 485, 513, 541, 569, 597, 625, 653]
-flags = []
-bombs = []
-ones = []
-twos = []
-threes = []
-fours = []
-fives = []
-sixes = []
-sevens = []
-eights = []
-empties = []
-known = []
-new_fields = [(461, 429)]
-clicked = True
 first_round = True
 end_game = False
-no_more_moves = False
 
-while not keyboard.is_pressed('k'):
+while True:
     f.leftClick(474, 174)
     f.leftClick(461, 429)
     time.sleep(0.2)
-    while not keyboard.is_pressed('l'): # Press l to generate a new starting position / Press and hold k and then press l to use the given starting position
-        time.sleep(0.05)
+    while not keyboard.is_pressed('l') and not keyboard.is_pressed('k'): # Press l to generate a new starting position / Press k to use the given starting position
+        time.sleep(0.02)
+    if keyboard.is_pressed('k'):
+        break
 
 while True:
-    img = pyautogui.screenshot("field.png")
+    no_more_moves = True
+    flags = []
+    bombs = []
+    ones = []
+    twos = []
+    threes = []
+    fours = []
+    fives = []
+    sixes = []
+    sevens = []
+    eights = []
+    empties = []
+    known = []
+    img_gui = pyautogui.screenshot("field.png")
+    img = Image.open('field.png').load()
     pyautogui.moveTo(1000, 500)
-    for pi in new_fields:
-        col = f.color(img, pi[0], pi[1])
-        if col == "0":
-            empties.append(pi)
-        elif col == "1":
-            ones.append(pi)
-        elif col == "2":
-            twos.append(pi)
-        elif col == "3":
-            threes.append(pi)
-        elif col == "4":
-            fours.append(pi)
-        elif col == "5":
-            fives.append(pi)
-        elif col == "6":
-            sixes.append(pi)
-        elif col == "7":
-            sevens.append(pi)
-        elif col == "8":
-            eights.append(pi)
-        known.append(pi)
 
-    if not first_round:
-        if clicked:
-            f.scan_field(img, x_pos, y_pos, known, empties, ones, twos, threes, fours, fives, sixes, sevens, eights, new_fields)
-    else:
-        for y in y_pos:
-            for x in x_pos:
-                if not (x, y) in known:
-                    col = f.color(img, x, y)
-                    if col == "0":
-                        empties.append((x, y))
-                    elif col == "1":
-                        ones.append((x, y))
-                    elif col == "2":
-                        twos.append((x, y))
-                    elif col == "3":
-                        threes.append((x, y))
-                    elif col == "4":
-                        fours.append((x, y))
-                    elif col == "5":
-                        fives.append((x, y))
-                    elif col == "6":
-                        sixes.append((x, y))
-                    elif col == "7":
-                        sevens.append((x, y))
-                    elif col == "8":
-                        eights.append((x, y))
+    field = f.scan_field(img, x_pos, y_pos, known, empties, ones, twos, threes, fours, fives, sixes, sevens, eights, flags)
+    for row in field:
+        print(row)
+    print("")
+    print("")
+
     known_fields = known.copy()
-
-    clicked = False
-    known = flags + ones + twos + threes + fours + fives + sixes + sevens + eights + empties
-    known = list(dict.fromkeys(known))
-    known.sort()
 
     for C in known.copy():
         num_of_neighbors = 0
         if not C in flags and not C in empties:
             num_of_neighbors = int(f.type(C[0], C[1], known, ones, twos, threes, fours, fives, sixes, sevens, eights, empties, flags))
             n = f.flags(f.neighbors(C[0], x_pos, C[1], y_pos), num_of_neighbors, known, flags)
-
-            if n[1]:
-                flags += n[0]
-                known += n[0]
-
-    new_fields = []
 
     for C in known.copy():
         num_of_neighbors = 0
@@ -107,21 +57,13 @@ while True:
             num_of_neighbors = int(f.type(C[0], C[1], known, ones, twos, threes, fours, fives, sixes, sevens, eights, empties, flags))
             n = f.no_mines(f.neighbors(C[0], x_pos, C[1], y_pos), known, flags, num_of_neighbors)
 
-            new_fields += n[0]
-            if n[1]:
-                clicked = True
-                no_more_moves = False
-
-    if known == known_fields and no_more_moves:
+    if known == known_fields:
         print("Taking guess:")
         guess = f.guess(x_pos, y_pos, known, empties, flags, ones, twos, threes, fours, fives, sixes, sevens, eights)
         try:
             f.leftClick(guess[0], guess[1])
         except:
             pass
-        break
-    elif known == known_fields:
-        no_more_moves = True
     if keyboard.is_pressed('q'):
         break
     first_round = False
